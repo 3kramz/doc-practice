35c5c7a1bc600f51f5d69be4d0bc936504710682
Ekramz <smahmud1098@gmail.com>
2023-06-26 11:48:43 +0600

    setup 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import './App.css';
 import Navbar from './Pages/Shared/Navbar/Navbar';
 import Home from './Pages/Home/Home';
 import { Routes, Route } from 'react-router-dom';
 import About from './Pages/About/About';
+import Appointment from './Pages/Appointment/Appointment';
+import Reviews from './Pages/Reviews/Reviews';
+import Contacts from './Pages/Contacts/Contacts';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar />
       
 
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/appointment" element={<Appointment />} />
+      <Route path="/reviews" element={<Reviews />} />
+      <Route path="/contact" element={<Contacts />} />
+      <Route path="/login" element={<Login />} />
+      
     </Routes>
 
     </div>
